e18460921bf19ae82a8e9b414e75e48e9ce8dbbb
lpezet <lpezet@gmail.com>
2024-07-21 18:47:59 +0200

    fix: fixed projectA getHello().

--- projectA/src/app.service.ts
+++ projectA/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Good 123 456!';
+    return 'Hello World Good!';
   }
 }
