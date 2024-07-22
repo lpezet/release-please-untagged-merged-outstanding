e68b7ab61f576307b0f2eb2993d25a06d0c2472a
lpezet <lpezet@gmail.com>
2024-07-22 21:38:27 +0200

    fix: fixed projectB App.

--- projectB/src/App.js
+++ projectB/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Here
+          Learn 213827 React
         </a>
       </header>
     </div>
